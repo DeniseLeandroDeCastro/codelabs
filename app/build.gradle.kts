19a8b11cb7de58f0e868ffcc05e9f681973215b3
Denise Castro <dmaria.natal@gmail.com>
2024-08-30 09:07:21 -0300

    30.08.2024 -> Adiciona o pacote util e arquivos de extensões

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -33,15 +34,96 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
+    //+--------------------------------------------------------------------------------------------+
+    //| LIBS                                                                                       |
+    //+--------------------------------------------------------------------------------------------+
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.legacy.support.v4)
+
+    //+--------------------------------------------------------------------------------------------+
+    //| MATERIAL COMPONENTS                                                                        |
+    //+--------------------------------------------------------------------------------------------+
+
+    implementation(libs.volley)
+    implementation(libs.gson)
+
+    //+--------------------------------------------------------------------------------------------+
+    //| CODELAB APP SETTINGS                                                                       |
+    //+--------------------------------------------------------------------------------------------+
+
+    implementation(libs.androidx.preference.ktx)
+
+    //+--------------------------------------------------------------------------------------------+
+    //| WORKMANAGER                                                                                 |
+    //+--------------------------------------------------------------------------------------------+
+
+    implementation(libs.androidx.work.runtime.ktx)
+    implementation(libs.glide)
+    implementation(libs.androidx.lifecycle.extensions)
+
+    //+--------------------------------------------------------------------------------------------+
+    //| VIEWPAGER                                                                                  |
+    //+--------------------------------------------------------------------------------------------+
+
+    implementation(libs.androidx.viewpager2)
+
+    //+--------------------------------------------------------------------------------------------+
+    //| NAVIGATION                                                                                 |
+    //+--------------------------------------------------------------------------------------------+
+
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.navigation.dynamic.features.fragment)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.work.runtime.ktx)
+
+    //+--------------------------------------------------------------------------------------------+
+    //| LIFECYCLE                                                                                  |
+    //+--------------------------------------------------------------------------------------------+
+
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.common.java8)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    //+--------------------------------------------------------------------------------------------+
+    //| COROUTINES                                                                                 |
+    //+--------------------------------------------------------------------------------------------+
+
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.coroutines.core)
+
+    //+--------------------------------------------------------------------------------------------+
+    //| ROOM                                                                                       |
+    //+--------------------------------------------------------------------------------------------+
+
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.sqlite.jdbc)
+    ksp(libs.androidx.room.compiler)
+
+    //+--------------------------------------------------------------------------------------------+
+    //| DEPURAÇÃO                                                                                  |
+    //+--------------------------------------------------------------------------------------------+
+
+    implementation(libs.timber)
+
+    //+--------------------------------------------------------------------------------------------+
+    //| TESTS                                                                                      |
+    //+--------------------------------------------------------------------------------------------+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
